4c4c46bab00ffeba24e319a58fcd229d30962296
monicaep <mep823@gmail.com>
2019-04-08 09:05:32 -0700

    Working with Node Modules: code challenge completed

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const terminalCommands = require('./terminal-commands');
 
 terminalCommands.ls();
+terminalCommands.touch('testFile');
+terminalCommands.mkdir('testDir');
--- terminal-commands.js
+++ terminal-commands.js
@@ -9,3 +9,19 @@ module.exports.ls = () => {
     console.log(filesToString);
   });
 };
+
+module.exports.touch = (fileName) => {
+  fs.writeFile(fileName, null, (err) => {
+    if (err) throw err;
+
+    console.log(`${fileName} created`)
+  });
+};
+
+module.exports.mkdir = (dirName) => {
+  fs.mkdir(dirName, (err) => {
+    if (err) throw err;
+
+    console.log(`${dirName} created`)
+  });
+};
