a1ad1fc9794109bc65cb27e21ba758c823dafec6
monicaep <mep823@gmail.com>
2019-04-08 09:08:49 -0700

    updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const terminalCommands = require('./terminal-commands');
 
 terminalCommands.ls();
-terminalCommands.touch('testFile');
-terminalCommands.mkdir('testDir');
+terminalCommands.touch(testFile.txt);
+terminalCommands.mkdir(testDir);
